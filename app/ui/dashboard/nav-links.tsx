5af1c8de59aea58ca133e5ce73a2c5df8f2ffd10
#55263025 <hardilpg123@gmail.com>
2023-12-23 14:37:54 +0700

    first commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,15 @@
+'use client'; // adding materi 5
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
   AtSymbolIcon // adding materi 4
 } from '@heroicons/react/24/outline';
+import Link from 'next/link'; // adding materi 5 // tag <Link>
+import { usePathname } from 'next/navigation'; // adding materi 5
+import clsx from 'clsx'; // adding materi 5
+
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -20,19 +26,28 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+  
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          // adding materi 5 
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
+              className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
+            >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </a>
+            {/* hidden teks md block */}
+            <p className="hidden md:block">{link.name}</p> 
+          </Link>
         );
       })}
     </>
